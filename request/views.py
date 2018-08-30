17f3231b4acd307b3903af6dbb1b192310ea15d6
DESKTOP-MB0RN9G\Nalbi_Min_Winodw <pyopie@nalbi.ai>
2018-08-30 14:28:13 +0900

    first update commit

--- request/views.py
+++ request/views.py
@@ -3,4 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
+    for f in request.FILES.getlist('files_video'):
+        print(f.name)
     return render(request, 'request/layout_request.html',)
